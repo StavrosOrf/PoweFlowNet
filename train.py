9f3446802c0c8eb156b2ed6d8fadb9db5bb9e801
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2023-06-08 15:31:27 +0200

    Added wandb logging and updated arparser

--- train.py
+++ train.py
@@ -14,6 +14,9 @@ from utils.argument_parser import argument_parser
 from utils.training import train_epoch, append_to_json
 from utils.evaluation import evaluate_epoch
 
+import wandb
+
+
 def main():
     # Step 0: Parse Arguments and Setup
     args = argument_parser()
@@ -23,46 +26,56 @@ def main():
     TRAIN_LOG_PATH = os.path.join(LOG_DIR, 'train_log/train_log_'+run_id+'.pt')
     SAVE_LOG_PATH = os.path.join(LOG_DIR, 'save_logs.json')
     SAVE_MODEL_PATH = os.path.join(SAVE_DIR, 'model_'+run_id+'.pt')
-    
+
     num_epochs = args.num_epochs
-    
     loss_fn = torch.nn.MSELoss()
-    lr = 1e-3
-    
+    lr = args.lr
+    batch_size = args.batch_size
+    grid_case = args.case
+
+    log_to_wandb = args.wandb
+    if log_to_wandb:
+        wandb.init(project="PowerFlowNet",
+                   entity="PowerFlowNet",
+                   name=run_id,
+                   config=args)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.manual_seed(1234)
     np.random.seed(1234)
     # torch.backends.cudnn.deterministic = True
     # torch.backends.cudnn.benchmark = False
-    
+
     # Step 1: Load data
-    trainset = PowerFlowData(root='data', case='14', split=[.5, .2, .3], task='train')
-    valset = PowerFlowData(root='data', case='14', split=[.5, .2, .3], task='val')
-    testset = PowerFlowData(root='data', case='14', split=[.5, .2, .3], task='test')
-    train_loader = DataLoader(trainset, batch_size=128, shuffle=True)
-    val_loader = DataLoader(valset, batch_size=128, shuffle=False)
-    test_loader = DataLoader(testset, batch_size=128, shuffle=False)
-    
+    trainset = PowerFlowData(root='data', case=grid_case,
+                             split=[.5, .2, .3], task='train')
+    valset = PowerFlowData(root='data', case=grid_case,
+                           split=[.5, .2, .3], task='val')
+    testset = PowerFlowData(root='data', case=grid_case,
+                            split=[.5, .2, .3], task='test')
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(valset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
+
     # Step 2: Create model and optimizer (and scheduler)
     node_in_dim, node_out_dim, edge_dim = trainset.get_data_dimensions()
     model = MPN(
-        nfeature_dim=node_in_dim, 
-        efeature_dim=edge_dim, 
-        output_dim=node_out_dim, 
-        hidden_dim=64, 
-        n_gnn_layers=3, 
-        K=3, 
+        nfeature_dim=node_in_dim,
+        efeature_dim=edge_dim,
+        output_dim=node_out_dim,
+        hidden_dim=16,
+        n_gnn_layers=2,
+        K=2,
         dropout_rate=0.5
-    ).to(device) # 40k params
+    ).to(device)  # 40k params
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 
-                                                           mode='min', 
-                                                           factor=0.5, 
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                           mode='min',
+                                                           factor=0.5,
                                                            patience=5,
                                                            verbose=True)
-    
-    # Step 3: Train model
-    # TODO: Add checkpoint
+
+    # Step 3: Train model    
     best_val_loss = 10000.
     saved_test_loss = 10000.
     train_log = {
@@ -74,36 +87,40 @@ def main():
             'loss': []}
     }
     for epoch in tqdm(range(num_epochs), position=0, leave=True):
-        train_loss = train_epoch(model, train_loader, loss_fn, optimizer, device)
+        train_loss = train_epoch(
+            model, train_loader, loss_fn, optimizer, device)
         val_loss = evaluate_epoch(model, val_loader, loss_fn, device)
         test_loss = evaluate_epoch(model, test_loader, loss_fn, device)
         scheduler.step(val_loss)
         train_log['train']['loss'].append(train_loss)
         train_log['val']['loss'].append(val_loss)
         train_log['test']['loss'].append(test_loss)
-        print(f"Epoch {epoch+1}/{num_epochs}: train_loss={train_loss:.4f}, val_loss={val_loss:.4f}, test_loss={test_loss:.4f}")
-        
-        # if epoch >= 8 * num_epochs // 10:
-        if True:
-            # if val_loss < 0.95 * best_val_loss:
-            if True:
-                best_val_loss = val_loss
-                saved_test_loss = test_loss
-                if args.save:
-                    _to_save = {
-                        'epoch': epoch,
-                        'args': args,
-                        'val_loss': best_val_loss,
-                        'test_loss': test_loss,
-                        'model_state_dict': model.state_dict(),
-                    }
-                    os.makedirs('models', exist_ok=True)
-                    torch.save(_to_save, SAVE_MODEL_PATH)
-        
+        print(
+            f"Epoch {epoch+1}/{num_epochs}: train_loss={train_loss:.4f}, val_loss={val_loss:.4f}, test_loss={test_loss:.4f}")
+        if log_to_wandb:
+            wandb.log({'train_loss': train_loss,
+                      'val_loss': val_loss, 'test_loss': test_loss})
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            saved_test_loss = test_loss
+            if args.save:
+                _to_save = {
+                    'epoch': epoch,
+                    'args': args,
+                    'val_loss': best_val_loss,
+                    'test_loss': test_loss,
+                    'model_state_dict': model.state_dict(),
+                }
+                os.makedirs('models', exist_ok=True)
+                torch.save(_to_save, SAVE_MODEL_PATH)
+
+        print(f"Best validation loss: {best_val_loss:.4f}")
+
     # Step 4: Evaluate model
     test_loss = evaluate_epoch(model, test_loader, loss_fn, device)
-    print(f"Final Test loss: {test_loss:.4f}")
-    
+    print(f"Test loss: {best_val_loss:.4f}")
+
     # Step 5: Save results
     if args.save:
         os.makedirs(os.path.join(LOG_DIR, 'train_log'), exist_ok=True)
@@ -117,8 +134,8 @@ def main():
                 'saved_file': SAVE_MODEL_PATH,
             }
         )
-    torch.save(train_log, TRAIN_LOG_PATH)
-    
-    
+        torch.save(train_log, TRAIN_LOG_PATH)
+
+
 if __name__ == '__main__':
     main()
--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -13,7 +13,10 @@ def argument_parser():
     parser.add_argument('--num_epochs', type=int, default=100, help='Number of epochs to train for')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('--case', type=str, default='14', help='Grid case')
+    parser.add_argument('--wandb', default=True, help='Enable wandb logging',action=argparse.BooleanOptionalAction)
     parser.add_argument('--save', default=False, action=argparse.BooleanOptionalAction)
+    
     
     args, left_argv = parser.parse_known_args()
     
