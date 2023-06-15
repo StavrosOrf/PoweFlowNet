5aa9de0ce1ed6ab668749c225c7d0e52058e0be2
Nathan Ordonez <nathanaxcan@gmail.com>
2023-06-15 12:34:48 +0200

    bugfix on the command

--- runs.sh
+++ runs.sh
@@ -3,14 +3,14 @@
 
 # this one gets 0.09 accuracy after 100 epochs
 # WITH THIS ONE USE MODEL MPN_simplenet
-python train.py --num_epochs 10\
-                --batch_size 128\
+python train.py --num-epochs 10\
+                --batch-size 128\
                 --lr 0.001\
                 --case 118\
                 --nfeature_dim 16\
                 --efeature_dim 5\
                 --hidden_dim 64\
                 --n_gnn_layers 2\
-                --k 3\
+                --K 3\
                 --dropout_rate 0.2\
                 --model MPN_simplenet
