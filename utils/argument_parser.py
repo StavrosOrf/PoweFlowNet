c3eb6b6c11a290ea4e32c6e7440c380f11cdf722
Nan <nanlin1997@outlook.com>
2023-06-27 12:39:57 +0000

    argument option for loss_fn - mse_loss - masked_l2 (default) - power_imbalance

--- utils/argument_parser.py
+++ utils/argument_parser.py
@@ -34,7 +34,7 @@ def argument_parser():
     parser.add_argument('--data-dir', type=str, default='data', help='Path to data directory')
     parser.add_argument('--train_loss_fn', 
                         type=str, default='masked_l2', 
-                        choices=['masked_l2', 'power_imbalance'],
+                        choices=['masked_l2', 'power_imbalance', 'mse_loss'],
                         help='Training loss function')
     parser.add_argument('--num-epochs', type=int, default=100, help='Number of epochs to train for')
     parser.add_argument('--batch-size', type=int, default=128, help='Batch size')
