b42fcedf633217efefa0deb884783201427d9d47
Stavros Orfanoudakis <stayros_1@yahoo.gr>
2023-08-04 17:41:31 +0200

    Implementing the errror per feature per node Plots

---
+++ error_per_feature.py
@@ -0,0 +1,259 @@
+"""
+This script is used to generate error per feature plots and statistics for the MPN model.
+"""
+
+import os
+
+import torch
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+import matplotlib.colors as mcolors
+import numpy as np
+import time
+
+from datasets.PowerFlowData import PowerFlowData
+from networks.MPN import MaskEmbdMultiMPN
+from utils.custom_loss_functions import Masked_L2_loss
+
+LOG_DIR = 'logs'
+SAVE_DIR = 'models'
+
+os.makedirs('./results', exist_ok=True)
+
+feature_names_output = [
+    'Voltage Magnitude (p.u.)',    # --- we care about this
+    'Voltage Angle (degrees)',        # --- we care about this
+    'Active Power (kW)',         # --- we care about this
+    'Reactive Power (kVar)',       # --- we care about this
+    'Gs',                   # -
+    'Bs'                    # -
+]
+
+GET_RESULTS = False
+sample_number = 50000
+cases = ['case14', 'case118', 'case6470rte']
+cases = ['case14', 'case118']
+
+if GET_RESULTS:
+
+    for case in cases:
+
+        case_name = case.split("case")[1]
+
+        print(f'\n\nCase {case_name} is being evaluated...')
+        # Load testing data
+        testset = PowerFlowData(root="./data/", case=case_name+"v2",
+                                split=[.5, .2, .3], task='test')
+
+        if sample_number > len(testset):
+            sample_number = len(testset)
+        print(f'Number of samples: {sample_number}')
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        eval_loss_fn = Masked_L2_loss(regularize=False)
+
+        # Load MPN model
+        model_path = "./models/testing/mpn_" + case_name + ".pt"
+
+        MPN_model = MaskEmbdMultiMPN(
+            nfeature_dim=6,
+            efeature_dim=5,
+            output_dim=6,
+            hidden_dim=129,
+            n_gnn_layers=4,
+            K=3,
+            dropout_rate=0.2
+        ).to(device)
+
+        _to_load = torch.load(model_path, map_location=device)
+        MPN_model.load_state_dict(_to_load['model_state_dict'])
+        MPN_model.eval()
+
+        # Get loss of MPN model and execution time
+        timer_MPN = 0
+        loss_MPN = 0
+
+        preds = []
+        targets = []
+        masks = []
+        types = []
+
+        for i, sample in enumerate(testset[:sample_number]):
+            time_start_gnn = time.time()
+            result = MPN_model(sample.to(device))
+
+            preds.append(result.to(device))
+            targets.append(sample.y.to(device))
+            masks.append(sample.x[:, 10:].to(device))
+            types.append(
+                np.argmax(np.array(sample.x[:, :4].to(device)), axis=1))
+
+            time_end_gnn = time.time()
+            loss_MPN += eval_loss_fn(result, sample.y.to(device),
+                                     sample.x[:, 10:].to(device)).item()
+
+            timer_MPN += time_end_gnn - time_start_gnn
+
+        print(f'Loss of MPN model: {loss_MPN/sample_number}')
+        print(f'Execution time of MPN model: {timer_MPN/sample_number}')
+
+        mean = testset.xymean[0].to(device)
+        std = testset.xystd[0].to(device)
+
+        preds = torch.stack(preds, dim=0) * std + mean
+        targets = torch.stack(targets, dim=0) * std + mean
+        error = preds - targets
+
+        error = error.detach().cpu().numpy()
+        masks = torch.stack(masks, dim=0).detach().cpu().numpy()
+        types = np.array(types)
+
+        masks = masks[:, :, :4]
+        errors = error[:, :, :4]
+        # Save results
+        print(f'masks shape: {masks.shape}')
+        print(f'types shape: {types.shape}')
+        print(f'error shape: {errors.shape}')
+
+        with open('./results/'+case_name+'_masks.npy', 'wb') as f:
+            np.save(f, masks)
+        with open('./results/'+case_name+'_types.npy', 'wb') as f:
+            np.save(f, types)
+        with open('./results/'+case_name+'_errors.npy', 'wb') as f:
+            np.save(f, errors)
+
+
+# Plot results
+cases = ['case14']
+for case in cases:
+    case_name = case.split("case")[1]
+    # load results
+    number = 100000
+    errors = np.load('./results/'+case_name+'_errors.npy')[:number, :, :]
+    masks = np.load('./results/'+case_name+'_masks.npy')[:number, :]
+    types = np.load('./results/'+case_name+'_types.npy')[:number]
+    # print(types,masks)
+
+    print(f'Number of Loads: {np.sum(types[0,:]==2)}')
+    print(f'Number of Generators: {np.sum(types[0,:]==1)}')
+
+    # get indexes of loads and generators
+    load_idx = np.where(types[0, :] == 2)[0]
+    gen_idx = np.where(types[0, :] == 1)[0]
+
+    # get error per feature
+    # fig, axes = plt.subplots(2, 2, figsize=(22, 16))
+
+    errors_loads = errors[:, load_idx, :]
+    errors_gens = errors[:, gen_idx, :]
+
+    error_per_feature = errors.reshape(-1, 4)
+    error_per_feature_loads = errors_loads.reshape(-1, 4)
+    error_per_feature_gens = errors_gens.reshape(-1, 4)
+
+    if False:
+        plt.style.use('seaborn-darkgrid')
+        fig, axes = plt.subplots(2, 2, figsize=(10, 7), tight_layout=True)
+        for idx, ax in enumerate(axes.flatten()):
+
+            N_all, bin_all, _ = ax.hist(error_per_feature[:, idx],
+                                        bins=100,
+                                        alpha=0.3,
+                                        label='All Nodes',
+                                        density=True)
+            N_loads, bin_loads, _ = ax.hist(error_per_feature_loads[:, idx],
+                                            bins=100,
+                                            alpha=0.3,
+                                            label='Load Nodes',
+                                            density=True)
+            N_gens, bin_gens, _ = ax.hist(error_per_feature_gens[:, idx],
+                                          bins=100,
+                                          alpha=0.3,
+                                          label='Generator Nodes',
+                                          density=True)
+
+            ax.set_xlabel(f'{feature_names_output[idx]}')
+            ax.set_ylabel('Probability')
+            ax.legend()
+            ax.set_yticks([])
+            # max_N = max(max(N_all/N_all.sum()), max(N_loads/N_loads.sum()),
+            #             max(N_gens/N_gens.sum()))
+            # max_N_value = max(max(N_all), max(N_loads), max(N_gens))
+            # ax.set_yticks([0,max_N_value], [0, max_N])
+            # ax.yaxis.set_major_formatter(PercentFormatter(xmax=100))
+
+        plt.savefig('./results/'+case_name+'error_distribution_'+'.png')
+        plt.show()
+
+    # plt.style.use('seaborn-darkgrid')
+    plt.subplots(2, 2, figsize=(10, 7), tight_layout=True)
+    print(errors.shape)
+    n_nodes = errors.shape[1]
+    n_bins = 100
+    n_values_to_print_y = 5
+    # Plot error per node average histogram
+    # for n in range(errors.shape[0]):
+
+    error_per_node_all = np.zeros((n_bins, n_nodes, 4))
+    # error_per_node_loads = errors[n, load_idx, :].reshape(-1, 4)
+    # error_per_node_gens = errors[n, gen_idx, :].reshape(-1, 4)
+    for i in range(4):
+        plt.subplot(2, 2, i+1)
+
+        min_value = np.min(errors[:, :, i])
+        max_value = np.max(errors[:, :, i])
+
+        if abs(min_value) >= max_value:
+            max_value = abs(min_value)
+
+        print(f'min_value: {min_value}, max_value: {max_value}')
+
+        bin_list = np.linspace(min_value, max_value, n_bins+1)
+        bin_list_print = np.linspace(min_value, max_value, n_values_to_print_y)
+
+        for n in range(n_nodes):
+            hist, bins = np.histogram(errors[:, n, i],
+                                      bins=bin_list,
+                                      density=True)
+            # print(hist.shape)
+            # print(bins.shape)
+            error_per_node_all[:, n, i] = hist
+
+        print(error_per_node_all[:, :, i].shape)
+        print(error_per_node_all[:, :, i])
+
+        plt.imshow(error_per_node_all[:, :, i].T,
+                   interpolation='nearest',
+                   aspect='auto',
+                   norm=mcolors.PowerNorm(0.3),
+                   cmap='jet',)
+        plt.colorbar(label='Probability')
+        plt.xticks(np.linspace(0, n_bins,n_values_to_print_y),
+                   np.round(bin_list_print, 4))
+        plt.xlabel('Nodes')
+        plt.ylabel('Error')
+
+    # for idx, ax in enumerate(axes.flatten()):
+    #     print(error_per_node_all[:,idx])
+    #     ax.hist2d(error_per_node_all[:, idx],
+
+    #                 bins=(100,n_nodes),
+    #                 norm=mcolors.PowerNorm(0.3),
+    #                 density=True,
+    #                 )
+    #     # ax.set_xlabel(f'{feature_names_output[idx]}')
+    #     ax.set_xlabel(f'{feature_names_output[idx]}')
+    #     ax.set_ylabel('Nodes')
+
+        # fig.colorbar(ax.hist2d(error_per_node_all[:, idx],
+        #                        np.arange(error_per_node_all.shape[0]),
+        #                        bins=100,
+        #                        norm=mcolors.PowerNorm(0.3))[3],
+        #              ax=ax,
+        #              label="Error Density")
+
+        # ax.legend()
+
+    plt.savefig('./results/'+case_name+'error_distribution_per_node_'+'.png')
+    # plt.show()
